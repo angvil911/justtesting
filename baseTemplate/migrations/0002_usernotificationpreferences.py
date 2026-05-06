c61cd796feddc9d8634049963685375b36152dfa
angvil911 <mbrmjalalab@gmail.com>
2026-05-06 13:00:05 +0100

    Fix migration dependency missing 0001_initial in baseTemplate

--- baseTemplate/migrations/0002_usernotificationpreferences.py
+++ baseTemplate/migrations/0002_usernotificationpreferences.py
@@ -10,7 +10,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('baseTemplate', '0001_initial'),
     ]
 
     operations = [
