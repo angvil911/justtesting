5190ba6d9d1d86b60c30cdb538069e077f0016b3
angvil911 <mbrmjalalab@gmail.com>
2026-05-07 15:52:51 +0100

    Add INSTALL_DIR variable and include git in dependencies

--- install.sh
+++ install.sh
@@ -7,62 +7,63 @@ REPO_USER="angvil911"
 REPO_NAME="justtesting"
 BRANCH="main"
 RAW_BASE="https://raw.githubusercontent.com/${REPO_USER}/${REPO_NAME}/${BRANCH}"
+INSTALL_DIR="/root/cyberpanel"
 
 OUTPUT=$(cat /etc/*release)
 if  echo $OUTPUT | grep -q "CentOS Linux 7" ; then
         echo "Checking and installing curl and wget"
-yum install curl wget -y 1> /dev/null
-yum update curl wget ca-certificates -y 1> /dev/null
+yum install curl wget git -y 1> /dev/null
+yum update curl wget git ca-certificates -y 1> /dev/null
                 SERVER_OS="CentOS"
 elif echo $OUTPUT | grep -q "CentOS Linux 8" ; then
         echo -e "\nDetecting Centos 8...\n"
         SERVER_OS="CentOS8"
-yum install curl wget -y 1> /dev/null
-yum update curl wget ca-certificates -y 1> /dev/null
+yum install curl wget git -y 1> /dev/null
+yum update curl wget git ca-certificates -y 1> /dev/null
 elif echo $OUTPUT | grep -q "AlmaLinux 8" ; then
         echo -e "\nDetecting AlmaLinux 8...\n"
         SERVER_OS="CentOS8"
-yum install curl wget -y 1> /dev/null
-yum update curl wget ca-certificates -y 1> /dev/null
+yum install curl wget git -y 1> /dev/null
+yum update curl wget git ca-certificates -y 1> /dev/null
 elif echo $OUTPUT | grep -q "AlmaLinux 9" ; then
         echo -e "\nDetecting AlmaLinux 9...\n"
         SERVER_OS="CentOS8"
-yum install curl wget -y 1> /dev/null
-yum update curl wget ca-certificates -y 1> /dev/null
+yum install curl wget git -y 1> /dev/null
+yum update curl wget git ca-certificates -y 1> /dev/null
 elif echo $OUTPUT | grep -q "AlmaLinux 10" ; then
         echo -e "\nDetecting AlmaLinux 10...\n"
         SERVER_OS="CentOS8"
-yum install curl wget -y 1> /dev/null
-yum update curl wget ca-certificates -y 1> /dev/null
+yum install curl wget git -y 1> /dev/null
+yum update curl wget git ca-certificates -y 1> /dev/null
 elif echo $OUTPUT | grep -q "CloudLinux 7" ; then
         echo "Checking and installing curl and wget"
-yum install curl wget -y 1> /dev/null
-yum update curl wget ca-certificates -y 1> /dev/null
+yum install curl wget git -y 1> /dev/null
+yum update curl wget git ca-certificates -y 1> /dev/null
                 SERVER_OS="CloudLinux"
 elif echo $OUTPUT | grep -q "CloudLinux 8" ; then
         echo "Checking and installing curl and wget"
-yum install curl wget -y 1> /dev/null
-yum update curl wget ca-certificates -y 1> /dev/null
+yum install curl wget git -y 1> /dev/null
+yum update curl wget git ca-certificates -y 1> /dev/null
                 SERVER_OS="CloudLinux"
 elif echo $OUTPUT | grep -q "Ubuntu 18.04" ; then
-apt install -y -qq wget curl
+apt install -y -qq wget curl git
                 SERVER_OS="Ubuntu"
 elif echo $OUTPUT | grep -q "Ubuntu 20.04" ; then
-apt install -y -qq wget curl
+apt install -y -qq wget curl git
                 SERVER_OS="Ubuntu"
 elif echo $OUTPUT | grep -q "Ubuntu 22.04" ; then
-apt install -y -qq wget curl
+apt install -y -qq wget curl git
                 SERVER_OS="Ubuntu"
 elif echo $OUTPUT | grep -q "openEuler 20.03" ; then
         echo -e "\nDetecting openEuler 20.03...\n"
         SERVER_OS="openEuler"
-yum install curl wget -y 1> /dev/null
-yum update curl wget ca-certificates -y 1> /dev/null
+yum install curl wget git -y 1> /dev/null
+yum update curl wget git ca-certificates -y 1> /dev/null
 elif echo $OUTPUT | grep -q "openEuler 22.03" ; then
         echo -e "\nDetecting openEuler 22.03...\n"
         SERVER_OS="openEuler"
-yum install curl wget -y 1> /dev/null
-yum update curl wget ca-certificates -y 1> /dev/null
+yum install curl wget git -y 1> /dev/null
+yum update curl wget git ca-certificates -y 1> /dev/null
 else
 
                 echo -e "\nUnable to detect your OS...\n"
@@ -72,9 +73,9 @@ fi
 
 # Clone the repo directly instead of downloading from CDN
 echo -e "\nDownloading DevaSystem installer...\n"
-rm -rf /root/cyberpanel
-git clone https://github.com/${REPO_USER}/${REPO_NAME}.git /root/cyberpanel
-cd /root/cyberpanel
+rm -rf "$INSTALL_DIR"
+git clone https://github.com/${REPO_USER}/${REPO_NAME}.git "$INSTALL_DIR"
+cd "$INSTALL_DIR" || exit
 
 # Run the main installer script
 chmod +x cyberpanel.sh
